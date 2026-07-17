f6ce9fca24a888142105a5bb15ee1849a225a9a1
mateohs18 <spinmhs@gmail.com>
2026-07-17 10:42:58 -0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'fortnite-api.com' },
+      { protocol: 'https', hostname: 'cdn.fortnite-api.com' },
       { protocol: 'https', hostname: 'cdn2.unrealengine.com' },
       { protocol: 'https', hostname: '*.supabase.co' },
       { protocol: 'https', hostname: 'cdn.discordapp.com' },
